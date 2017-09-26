ac86ffa08f8433aa4b7b8d0ff50b35736cb61ad5
GTaumaturgo <gtaumaturg@gmail.com>
2017-09-26 17:30:28 -0300

    adicionada funcao que calcula distância baseado em coordenadas

--- src/br/unb/exp/Calculator/Calculator.java
+++ src/br/unb/exp/Calculator/Calculator.java
@@ -1,11 +1,13 @@
 package br.unb.exp.Calculator;
 
+import br.unb.exp.Coordinate;
+
 import java.util.ArrayList;
 
 public class Calculator {
 
-    public double CalculateStdDeviation(ArrayList<Double> list){
-        double mean = CalculateMean(list);
+    public double calculateStdDeviation(ArrayList<Double> list){
+        double mean = calculateMean(list);
         double acc = 0.0;
         for(int i = 0; i < list.size(); i++){
             double aux = (mean - list.get(i));
@@ -16,12 +18,25 @@ public class Calculator {
         return Math.sqrt(acc);
     }
 
-    public double CalculateMean(ArrayList<Double> list)
+    public double calculateMean(ArrayList<Double> list)
     {
         double acc = 0;
         for (int i = 0; i < list.size();i++){
             acc += list.get(i);
         }
         return acc / list.size();
+    }
+
+    public double calculateDistance(Coordinate u, Coordinate v){
+        final double R = 6371.0;
+        double deltaLat = Math.toRadians(v.latitude - u.latitude);
+        double deltaLon = Math.toRadians(v.longitude - u.longitude);
+        double a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2)
+                + Math.cos(Math.toRadians(u.latitude)) * Math.cos(Math.toRadians(v.latitude))
+                * Math.sin(deltaLon / 2) * Math.sin(deltaLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+        return distance;
+
     }
 }
